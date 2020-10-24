9128407f9057b5912bfe40632e8062e52042cdee
andersonmavi30 <53633010+andersonmavi30@users.noreply.github.com>
2020-10-23 21:47:24 -0500

    Add files via upload

---
+++ python_devnet.py
@@ -0,0 +1,38 @@
+import getpass
+import telnetlib
+
+HOST = "172.20.30.1"
+user = input("Usuario: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"login: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"enable\n")
+tn.write(b"configure terminal\n")
+tn.write(b"hostname SWCACCESO1\n")
+tn.write(b"username anderson.martinez privilege 15 secret cisco\n")
+tn.write(b"vlan 10\n")
+tn.write(b"name TECNOLOGIA\n")
+tn.write(b"exit\n")
+tn.write(b"interface range GigabitEthernet 0/0-2\n")
+tn.write(b"switchport mode access\n")
+tn.write(b"switchport access vlan 30\n")
+tn.write(b"description TECNOLOGIA\n")
+tn.write(b"exit\n")
+tn.write(b"line console 0\n")
+tn.write(b"history size 256\n")
+tn.write(b"login local\n")
+tn.write(b"exec-timeout 3\n")
+tn.write(b"session-timeout 3\n")
+tn.write(b"session-limit 2\n")
+tn.write(b"logging synchronous\n")
+tn.write(b"exit\n")
+tn.write(b"do write memory\n")
+
+print(tn.read_all().decode('ascii'))
